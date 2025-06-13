55af2998b2e4d87fa8e38be35edf8b4e99f74013
Rodolfo Scarp <rodolfoscarp@gmail.com>
2025-06-12 21:03:48 -0300

    fix: atualização versão pyinstaller, correção splash e icone.

--- {{ cookiecutter.project_slug }}/src/utils/loading.py
+++ {{ cookiecutter.project_slug }}/src/utils/loading.py
@@ -4,7 +4,7 @@ import os
 
 
 def start_loading():
-    if "_PYIBoot_SPLASH" in os.environ and importlib.util.find_spec("pyi_splash"):
+    if getattr(sys, "frozen", False):
         import pyi_splash
 
         pyi_splash.update_text("Desenvovido por {{ cookiecutter.author_name }}.")
